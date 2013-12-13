0dd5df30f1ab519d0b4c3e603c66d71ff40e1807
n02469512 <n02469512@hawkmail.newpaltz.edu>
2013-12-13 00:53:06 -0500

    lighttpd PHP script

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+  print <<< EOT
+<!doctype html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>USB Security Camera System</title>
+</head>
+<body>
+<h1>N02469512 Security Camera System</h1>
+<p>Each stream is linked below, along with the corresponding archives for
+that camera:</p>
+<a href="https://stream1-n02469512.pagekite.me">Stream 1</a>
+<a href="https://archives1-n02469512.pagekite.me">Stream 1 - Archives</a>
+<br>
+<a href="https://stream2-n02469512.pagekite.me">Stream 2</a>
+<a href="https://archives2-n02469512.pagekite.me">Stream 2 - Archives</a>
+<br>
+</body>
+</html>
+EOT;
+
+?>
